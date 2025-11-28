f03a898a9e24f7a3e9fbfce6b1ad94d603dc39e3
Ashfaque Hossain Abir <ashfaquehossainabirr@gmail.com>
2025-11-28 16:29:38 +0600

    Wrap student data in a div for styling

--- src/components/StudentData.jsx
+++ src/components/StudentData.jsx
@@ -3,10 +3,12 @@ import React from "react"
 const StudentData = (data) => {
     return(
         <>
-            <h2>Student Data</h2>
-            <p>Data: { data.name }</p>
-            <p>Age: { data.age }</p>
-            <p>isStudent: { data.isStudent ? "Yes" : "No" }</p>
+            <div className="student-card">
+                <h2>Student Data</h2>
+                <p>Data: { data.name }</p>
+                <p>Age: { data.age }</p>
+                <p>isStudent: { data.isStudent ? "Yes" : "No" }</p>
+            </div>
         </>
     )
 }
